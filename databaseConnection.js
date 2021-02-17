6e24b9ff262d347051356e9769606bb15ed4ac18
matteo <matteo@matteos-MacBook-Pro.local>
2021-02-16 23:30:56 -0800

    connected heroku db

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,12 +2,13 @@ const mysql = require('mysql2');
 
 const is_heroku = process.env.IS_HEROKU || false;
 
+//mysql://b174a41e9846d2:3922fd99@us-cdbr-east-03.cleardb.com/heroku_95732b56673a61a?reconnect=true 
 //mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b6fb24cb4c8c37",
-	password: "702be0f5",
-	database: "heroku_a79dfbc72e06757",
+	user: "b174a41e9846d2",
+	password: "3922fd99",
+	database: "heroku_95732b56673a61a",
 	multipleStatements: false
 };
 
